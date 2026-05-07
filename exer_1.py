655a29e8a5f772485b5c4ea9153dd06ab3f6ad3b
Imbiridin <marcosimbiriba1@gmail.com>
2026-05-06 21:21:34 -0300

    Exercício 1 finalizado

--- exer_1.py
+++ exer_1.py
@@ -17,8 +17,21 @@ while True:
 
 media_produtos = sum(preco_produto) / len(preco_produto)
 
+caro = max(preco_produto)
 
+quantidade_caros = 0
+for preco in preco_produto:
+    if preco >= 100:
+        quantidade_caros += 1
+
+if quantidade_caros > 1:
+    print(f"Muitos produtos caros ({quantidade_caros} produtos)")
+
+print("-" * 50)
+print("O produto mais caro:", caro)
 print("Produtos cadastrados: ", nome_produto)
 print("Preços dos produtos: ", preco_produto)
-print("Media dos produtos: ", media_produtos)
+print("Media dos produtos: ", f"{media_produtos:.2f}")
+print("Produtos acima de R$100,00:", quantidade_caros)
 print("Total: ", total)
+print("-" * 50)
